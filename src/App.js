9bb785b6d230a207158f6fc804e10ea6d67de333
soaib <soaibtech@gmail.com>
2023-05-30 15:13:55 +0530

    navbar is created

--- src/App.js
+++ src/App.js
@@ -31,9 +31,18 @@ const App = () => {
                                 </Routes>
                             </div>
                     </Layout>
-                </div>
-                <div className="footer" >
-
+                
+                    <div className="footer">
+                        <Typography.Title level={5} style={{color: '#fff', textAlign: 'center'}}>
+                            Cryptoverse <br/>
+                            All rights reserved
+                        </Typography.Title>
+                        <Space>
+                            <Link to='/'>Home</Link>
+                            <Link to='/exchanges'>Exchanges</Link>
+                            <Link to='/news'>News</Link>    
+                        </Space>
+                    </div>
                 </div>
             </div>
         </>
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/coins',
+  params: {
+    referenceCurrencyUuid: 'yhjMzLPhuIDl',
+    timePeriod: '24h',
+    'tiers[0]': '1',
+    orderBy: 'marketCap',
+    orderDirection: 'desc',
+    limit: '50',
+    offset: '0'
+  },
+  headers: {
+    'X-RapidAPI-Key': '8b433c3fb4mshba8f57cd16c46b4p14317cjsn3ac2cd066941',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}

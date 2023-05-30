74d687a2f5020315b2e6ecbaf95798f4cd83db67
soaib <soaibtech@gmail.com>
2023-05-30 13:16:40 +0530

    project layout is craeted for crypto-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar,Homepage, Cryptocurrencies,CryptoDetails,News,Exchanges } from "./components";
 import './App.css';
+const App = () => {
+    return (
+        <>
+            <div className="app">
+                <div className="navbar">
+                    <Navbar />
+                </div>
+                <div className="main">
+                    <Layout>
+                            <div className="routes">
+                                <Routes>
+                                    <Route exact path="/" element={<Homepage/>}>
+                                    </Route>
+                                    <Route exact path="/exchanges" element={<Exchanges/>}>
+                                        
+                                    </Route>
+                                    <Route exact path="/cryptocurrencies" element={<Cryptocurrencies/>}>
+                                        
+                                    </Route>
+                                    <Route exact path="/crypto/:cryptoId" element={<CryptoDetails/>}>
+                                        
+                                    </Route>
+                                    <Route exact path="/news" element={ <News/>}>
+                                    
+                                    </Route>
+                                </Routes>
+                            </div>
+                    </Layout>
+                </div>
+                <div className="footer" >
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default App;

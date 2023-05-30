3f8dc8152d8e775161460446823bf6eae34fea23
soaib <soaibtech@gmail.com>
2023-05-30 18:16:22 +0530

    homepage with crypto and cryptopage is added

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,11 +3,12 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic} from 'antd';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import {Cryptocurrencies, News} from './';
 
 const {Title} = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
 
   const globalStats = data?.data?.stats;
 
@@ -24,8 +25,18 @@ const Homepage = () => {
         <Col span={12} > <Statistic title='Total Market Cap' value={ millify(globalStats.totalMarketCap)}></Statistic></Col>
         <Col span={12} > <Statistic title='Total 24h Volume' value={ millify(globalStats.total24hVolume)}></Statistic></Col>
         <Col span={12} > <Statistic title='Total Markets' value={ millify(globalStats.totalMarkets)}></Statistic></Col>
-
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className='home-title'>Top 10 Cryptocurrencies in  the world!</Title>
+        <Title level={3} className='show-more'><Link to='/cryptocurrencies'>show more</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+
+      <div className="home-heading-container">
+        <Title level={2} className='home-title'>latest Crypto News!</Title>
+        <Title level={3} className='show-more'><Link to='/news'>show more</Link></Title>
+      </div>
+      <News simplified />
     </>
   )
 }
